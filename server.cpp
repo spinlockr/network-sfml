7d7182486998d50d20d70dadcbdf7ec0fc1ebb01
coderjohn4 <89047848+coderjohn4@users.noreply.github.com>
2021-11-15 07:50:40 +1000

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -2,47 +2,8 @@
 #include <SFML/Network.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <string>
 using namespace std;
-
-class ClientObj
-{
-    
-    public: 
-
-        // sf::RenderWindow window;
-        int clientID = 0;
-
-        ClientObj(){    
-            // sf::RenderWindow* window = new window(sf::VideoMode(400, 400), "GAME X");
-            sf::RenderWindow window(sf::VideoMode(400, 400), "GAME X");
-        };
-    
-        void gameLoop()
-        {
-
-            while(window.isOpen())
-            {
-                sf::Event Event;
-
-                while(window.pollEvent(Event))
-                {
-                    if(Event.type == sf::Event::Closed || Event.key.code == sf::Keyboard::Escape)
-                    {
-                        window.close();
-                    }
-                    else if(Event.key.code == sf::Keyboard::Space)
-                    {
-                        cout << clientID << endl;
-                    }
-                }
-            
-                // window->draw();
-                window.display();
-                window.clear();
-            }
-
-        };
-};
 
 
 int main(){
@@ -50,22 +11,39 @@ int main(){
     sf::TcpListener listener;
     listener.listen(53000);
 
-    bool running = true;
-    // ClientObj* clients[10];
-    int totalClients = 0;
-    int clientNum = 0;
 
+    bool running = true;
     while (running)
     {
         sf::TcpSocket client;
+        sf::TcpSocket client1;
+        sf::TcpSocket client2;
+        
         if (listener.accept(client) == sf::Socket::Done)
         {   
-            std::cout << "New connection received from " << client.getRemoteAddress() << std::endl;
-            totalClients += 1;
-            clientNum += 1;        
-            ClientObj* client = new ClientObj();
-            client->clientID = clientNum;
-            client->gameLoop();
+            cout << "New connection received from " << client.getRemoteAddress() << endl;
+            while (running)
+            {   
+                string data;
+                sf::Packet packet;
+                client.receive(packet);
+                packet >> data;	
+                cout << data << endl;
+                packet.clear();
+            }
+        }
+        if (listener.accept(client1) == sf::Socket::Done)
+        {   
+            cout << "New connection received from " << client.getRemoteAddress() << endl;
+            while (running)
+            {   
+                string data;
+                sf::Packet packet;
+                client.receive(packet);
+                packet >> data;	
+                cout << data << endl;
+                packet.clear();
+            }
         }
     }
 
@@ -75,4 +53,62 @@ int main(){
 
     return 0;
 
+};
+
+//crtl b 0 to swap window 
+//ctrl b c to creat a window
+//~/.tmux.conf
+
+// void* message {new std::string};
+// sf::Packet packet;
+// client.receive(packet);
+// static_cast<std::string>(*message) = packet.getData();
+// std::cout << packet.getData() << std::endl;
+// std::cout << static_cast<std::string>(*message) << std::endl; // prints the data
+// std::cout << message << std::endl; // prints the memory location
+// packet.clear();
+
+
+// sf::Socket::Status status = client.recieve(packet);
+// if(status != sf::Socket::Status::Done){
+//   std::cout << "Socket was not done." << '\n' << status;
+// }
+// else{
+//   std::string str;
+//   if(!(packet >> str))
+//     std::cout << "Failed to read string into packet. Packet size: " << packet.getDataSize(); 
+//   else
+//     std::cout << str;
+// }
+
+
+// sf::Packet packet;
+// client.receive(packet);
+// std::string message((char*)packet.getData(),packet.getDataSize());
+// std::cout<<message;
+// packet.clear();
+
+// sf::Packet packet;
+// client.receive(packet);
+// std::string message((char*)packet.getData(),packet.getDataSize());
+// std::cout<<message;
+// packet.clear();
+
+
+// const void* message;
+// (string*)message;
+
+// string data;
+// sf::Packet packet;
+// client.receive(packet);
+// cout << packet <<   endl;
+// // message = 
+// packet.getData();
+// cout << packet.getData() << endl;
+
+// packet >> data;	
+// cout << data << endl;
+// // cout << message << endl;
+// // cout << *message << endl;
+// packet.clear();
 };

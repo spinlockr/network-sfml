77c6ed407ab4a6b128605f624485d37d1de32c92
coderjohn4 <89047848+coderjohn4@users.noreply.github.com>
2021-11-15 07:50:01 +1000

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,10 +1,29 @@
 #include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
 
 main(){
 
     sf::TcpSocket socket;
     sf::Socket::Status status = socket.connect("192.168.0.47", 53000);
-    sf::RenderWindow window(sf::VideoMode(400, 400), "GAME X");
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Client");
 
+    bool chatting = true;
+    string x;
+
+    cout << "Connected to host" << endl;
+
+    while(chatting)
+    {
+        cout << "Send a message: ";
+        cin >> x;
+        cout << x << endl;
+        sf::Packet packet;
+        packet << x ;
+        socket.send(packet);
+        packet.clear();
+
+    };
     
 };
